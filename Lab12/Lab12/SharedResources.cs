ed8a3bf88068300f8db9543c4085ba9f43353b33
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-06-07 11:19:09 -0500

    First Commit

---
+++ Lab12/Lab12/SharedResources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Lab12
+{
+	public static class SharedResources
+	{
+		public static Color OpButtonBkColor
+        {
+			get
+			{
+				return Color.FromRgb(0xff, 0xa5, 0);
+			}
+		}
+	}
+}
+
---
+++ Lab12/Lab12/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Xamarin.Forms;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Data;
+
+namespace Lab12.ViewModel
+{
+	public class CalculatorViewModel: INotifyPropertyChanged
+	{
+		private string _result;
+
+        public CalculatorViewModel()
+        {
+            CalculateCommand = new Command<string>(Calculate);
+            ClearCommand = new Command(Clear);
+            _result = string.Empty;
+        }
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public ICommand CalculateCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        private void Calculate(string parameter)
+        {
+            if (parameter == "=")
+            {
+                Result = new DataTable().Compute(Result, null).ToString();
+            }
+            else
+            {
+                Result += parameter;
+            }
+        }
+
+        private void Clear()
+        {
+            Result = string.Empty;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+	}
+}
+
